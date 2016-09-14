10939f5b35eaa891d97395819c1f8bc1bc91e6cd
sqljudo <russ.thomas@outlook.com>
2016-09-14 08:54:09 -0600

    Initial commit of script library
    This is a library of my most used scripts.  Some have been modified or downloaded from others.  When this is the case, origin and credit, is retained as much as possible.

---
+++ Backups/Backup History.sql
@@ -0,0 +1,26 @@
+
+/* -------------------------------------------------------------------------- 
+   Database Backups history for previous week including time it took to run
+-------------------------------------------------------------------------  */
+
+SELECT  CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS [server] ,
+        msdb.dbo.backupset.database_name ,
+        msdb.dbo.backupset.backup_start_date ,
+        msdb.dbo.backupset.backup_finish_date ,
+        DATEDIFF(mi, msdb.dbo.backupset.backup_start_date,
+                 msdb.dbo.backupset.backup_finish_date) AS [backup_time_minutes] ,
+        msdb.dbo.backupset.expiration_date ,
+        CASE msdb..backupset.[type]
+          WHEN 'D' THEN 'Database'
+          WHEN 'L' THEN 'Log'
+        END AS backup_type ,
+        msdb.dbo.backupset.backup_size ,
+        msdb.dbo.backupmediafamily.logical_device_name ,
+        msdb.dbo.backupmediafamily.physical_device_name ,
+        msdb.dbo.backupset.name AS backupset_name ,
+        msdb.dbo.backupset.[description]
+FROM    msdb.dbo.backupmediafamily
+        INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id
+WHERE   ( CONVERT(DATETIME, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 7 )
+ORDER BY msdb.dbo.backupset.database_name ,
+        msdb.dbo.backupset.backup_finish_date 
